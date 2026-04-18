cb0bce1349f894b1d9c79e949bfe7877a5df9cdf
Nalin Singh <154657904+NalinKumarSingh@users.noreply.github.com>
2026-04-19 01:46:32 +0530

    Change Redis connection from 'redis' to 'rediss'

--- src/main/java/com/example/flight/v1/configure/RedissonConfig.java
+++ src/main/java/com/example/flight/v1/configure/RedissonConfig.java
@@ -13,7 +13,7 @@ public class RedissonConfig {
   public RedissonClient redissonClient() {
     Config config = new Config();
     config.useSingleServer()
-        .setAddress("redis://default:gQAAAAAAAY0NAAIocDI3Yjc4M2JlM2VjNTU0MWE1YjNlZDA5MWNhYjhjNzM1NnAyMTAxNjQ1@magnetic-blowfish-101645.upstash.io:6379");
+        .setAddress("rediss://default:gQAAAAAAAY0NAAIocDI3Yjc4M2JlM2VjNTU0MWE1YjNlZDA5MWNhYjhjNzM1NnAyMTAxNjQ1@magnetic-blowfish-101645.upstash.io:6379");
     return Redisson.create(config);
   }
 }
